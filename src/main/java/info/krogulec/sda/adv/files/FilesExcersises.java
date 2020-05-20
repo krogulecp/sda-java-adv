dcdc629eecbb8c48cfab1089a3335f2d9c509d95
Piotr Krogulec <krogulec.piotr@gmail.com>
2020-05-21 00:02:29 +0200

    functional interfaces and files excersises

---
+++ src/main/java/info/krogulec/sda/adv/files/FilesExcersises.java
@@ -0,0 +1,48 @@
+package info.krogulec.sda.adv.files;
+
+import java.io.IOException;
+
+/**
+ * @author krogulecp
+ */
+class FilesExcersises {
+
+    private static final String TEST_NIO_PATH = "C:\\Path";
+
+    private static void createFile() {
+        //TODO Stworzyć plik nio.txt i zapisać w nim listę one, two, three
+    }
+
+    private static void readFile() {
+        //TODO odczytać zawartość za pomocą lines i readAllLines
+    }
+
+    private static void copyFile() {
+        //TODO skopiować plik do katalogu TESTII
+    }
+
+    private static void deleteFile() {
+        //TODO usunąć skopiowany plik nio.txt
+    }
+
+    private static void deleteNotExistingFile() {
+        //TODO spróbować usunąć nieistniejący plik
+    }
+
+    private static void moveFile() {
+        // TODO Przenieść plik nio.txt do TESTII
+    }
+
+    private static void copyDir() {
+        //TODO skopiować katalog TEST_NIO_PATH
+    }
+
+    private static void createDir() throws IOException {
+        //TODO stworzyć podkatalog w TEST_NIO_PATH o dowolnej nazwie
+    }
+
+    private static void moveDir() {
+        //TODO przenieść stworzony katalog do TESTII
+    }
+
+}
---
+++ src/main/java/info/krogulec/sda/adv/functionalinterfaces/functional_interface.md
@@ -0,0 +1,7 @@
+1. Stworzyć klasę Film z polami title, author;
+2. Stworzyć własny interfejs funkcyjny FilmMapper (posiada jedną metodę abstrakcyjną 
+   przyjmującą dowolny typ i zwracający Film)
+3. Stworzyć klasę FilmUser, która będzie miała metodę createFilm, która przyjmie FilmMappera
+   i title
+4. Stworzyć metodę main, która wywoła metodę createFilm i wypisze na konsolę film
+ 
